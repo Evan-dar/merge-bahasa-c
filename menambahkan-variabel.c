5352472ef9238a2b7258974640eb79d5b8b722a2
Evan-dar <evansdarmawans@gmail.com>
2024-12-23 19:35:46 +0700

    menambahkan variabel integer

--- menambahkan-variabel.c
+++ menambahkan-variabel.c
@@ -2,8 +2,13 @@
 
 int main(){
   char hrf[20];
-
+  int angka; 
+  
   printf("Masukan nama anda :");
   scanf("%c", &hrf);
+
+  printf("Masukan Umur anda sekarang :")
+  scanf("%i", &angka);
+  
   
 }
